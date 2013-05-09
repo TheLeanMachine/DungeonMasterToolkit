ac0ec5337230aec10038cd9b992cff7a2c057e50
Kai Hoelscher <myfatbackup@yahoo.de>
2013-05-09 19:17:42 +0200

    Integrate agular.js (along with the base for the char generator)

---
+++ js/CharacterGeneratorCtrl.js
@@ -0,0 +1,8 @@
+function CharacterGeneratorCtrl($scope) {
+  $scope.characters = [
+    { name: 'Gandalf', class: 'Magier', level: 20},
+    { name: 'Gimli', class: 'K&auml;mpfer', level: 18}
+  ];
+
+  $scope.foo = 'bar';
+}
