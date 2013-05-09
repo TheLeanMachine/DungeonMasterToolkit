da35d1c3453bb7f4285b27276151b20faa9e668a
Kai Hoelscher <myfatbackup@yahoo.de>
2013-05-09 21:41:41 +0200

    Major improvements

--- js/CharacterGeneratorCtrl.js
+++ js/CharacterGeneratorCtrl.js
@@ -3,28 +3,76 @@ function logError(errMsg) {
 }
 
 function CharacterGeneratorCtrl($scope) {
-  $scope.characters = [
-    { name: 'Gandalf', class: 'Magier', level: 20},
-    { name: 'Gimli', class: 'Kämpfer', level: 18}
+
+  $scope.availableLevels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+  /**
+   * List of available classes.
+   *
+   * @type {Array}
+   */
+  $scope.availableClasses = [
+// TODO 'availableClasses' should be a MAP of 'classId' -> 'className/Label'!
+// TODO introduce 'Character' "class" (no actual classes in JavaScript! Pure objects, baby...)
+    {id:'fighter', label:'KämXpfer'},
+    {id:'thief', label:'Dieb'},
+    {id:'mage', label:'Magier'},
+    {id:'priest', label:'Priester'}
   ];
 
   /**
-   * @param newCharacter the Character to create
+   * List of characters.
+   *
+   * @type {Array}
    */
-  $scope.createCharacter = function(newCharacter){
+  $scope.characters = [
+    { name: 'Gandalf', classId: 'mage', level: 20},
+    { name: 'Gimli', classId: 'fighter', level: 18}
+  ];
+
+  /**
+   * Creates a new character depending on form input.
+   *
+   * @param formCharacterModel the character model of the &lt;form&gt; input in the UI
+   */
+  $scope.createCharacter = function(formCharacterModel){
+    // make a copy from the form input (otherwise, changes in the form
+    // input would be reflected in the list of all characters!
+    var newCharacter = angular.copy(formCharacterModel);
     var requiredCharPropertyMissing;
 
     if (!newCharacter) {
-      logError("'newCharacter' is undefined (No charter object provided).");
+      logError("'newCharacter' is undefined (no form input given).");
       return;
     }
 
-    requiredCharPropertyMissing = !newCharacter.name || !newCharacter.class || !newCharacter.level;
+    requiredCharPropertyMissing = !newCharacter.name || !newCharacter.classId || !newCharacter.level;
     if (requiredCharPropertyMissing) {
       logError("Cannot create new character! Required property missing in 'newCharacter'.");
       return;
     }
 
+
     $scope.characters.push(newCharacter);
   };
+
+  /**
+   * Converts a character class ID to the class label.
+   *
+   * @param classId of the character class
+   */
+  $scope.classIdToClassLabel = function(classId) {
+    var i;
+    var numOfClasses = $scope.availableClasses.length;
+    var classes = $scope.availableClasses;
+    var currentClass;
+// TODO 'availableClasses' should be a MAP of 'classId' -> 'className/Label'!
+    for (i=0; i<numOfClasses; ++i) {
+      currentClass = classes[i];
+      if (currentClass.id === classId) {
+        return currentClass.label;
+      }
+    }
+    return "No appropriate character class found for classId 'classId'";
+  }
 }
