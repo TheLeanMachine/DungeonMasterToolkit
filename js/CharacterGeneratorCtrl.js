3946969d845cfd1b80ed896c23ad399cfedb305c
Kai Hoelscher <myfatbackup@yahoo.de>
2013-05-09 20:48:08 +0200

    Character list is extendable (two-way data-binding 4 free!)

--- js/CharacterGeneratorCtrl.js
+++ js/CharacterGeneratorCtrl.js
@@ -1,6 +1,30 @@
+function logError(errMsg) {
+  console.log('[ERROR][CharacterGeneratorCtrl]' + errMsg);
+}
+
 function CharacterGeneratorCtrl($scope) {
   $scope.characters = [
     { name: 'Gandalf', class: 'Magier', level: 20},
     { name: 'Gimli', class: 'Kämpfer', level: 18}
   ];
+
+  /**
+   * @param newCharacter the Character to create
+   */
+  $scope.createCharacter = function(newCharacter){
+    var requiredCharPropertyMissing;
+
+    if (!newCharacter) {
+      logError("'newCharacter' is undefined (No charter object provided).");
+      return;
+    }
+
+    requiredCharPropertyMissing = !newCharacter.name || !newCharacter.class || !newCharacter.level;
+    if (requiredCharPropertyMissing) {
+      logError("Cannot create new character! Required property missing in 'newCharacter'.");
+      return;
+    }
+
+    $scope.characters.push(newCharacter);
+  };
 }
