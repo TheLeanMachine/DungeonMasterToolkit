8d03381c6b042eba405d8f06c56360918c7598f1
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2013-05-22 14:56:20 +0200

    Add doc

--- js/CharacterGeneratorCtrl.js
+++ js/CharacterGeneratorCtrl.js
@@ -233,6 +233,12 @@ function CharacterGeneratorCtrl($scope) {
   // member properties - variables and functions - exported
   // in the Scope of the 'CharacterGeneratorCtrl'
   //
+
+  /**
+   * List of available, numerical levels.
+   *
+   * @type {Array}
+   */
   $scope.availableLevels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
   /**
