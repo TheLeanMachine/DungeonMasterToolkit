55bf38182fc3bfce18efcd231c5af0f235c919ce
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2013-05-22 17:01:21 +0200

    Reorder file contents: 1. Controller, 2. domain model and 3. Helper functions (no code changes!)

--- js/CharacterGeneratorCtrl.js
+++ js/CharacterGeneratorCtrl.js
@@ -1,78 +1,113 @@
 // ------------------------------------------------------------------------
-// Global Helper functions.
+// The controller for handling form input.
 // ------------------------------------------------------------------------
 
-function logError(error) {
-  console.log('[ERROR][CharacterGeneratorCtrl] ' + error.message);
-}
+function CharacterGeneratorCtrl($scope) {
+  //
+  // private controller state
+  //
 
-function timestamp() {
-  return new Date().getTime();
-}
+  /**
+   * Collection of characters created with this controller.
+   */
+  var characterCollection = (function(){ // Function gets executed immediately!
+    var result;
+    var gandalf = RULE_ENGINE.createCharacter('Gandalf', CharacterClass.prototype.CLASS_ID.mage, 20);
+    var gimli = RULE_ENGINE.createCharacter('Gimli', CharacterClass.prototype.CLASS_ID.fighter, 18);
+    var legolas = RULE_ENGINE.createCharacter('Legolas', CharacterClass.prototype.CLASS_ID.thief, 18);
 
-/**
- * @param potentialString Variable whose type gets checked
- * @returns {boolean} TRUE, if 'potentialString' is a 'String'
- */
-function isString(potentialString) {
-  // 'Object.prototype.toString(...)' is used here, since we want to use the
-  // native/ 'toString()' instead of a potentially existing, custom
-  // toString()-method!
-  return Object.prototype.toString.call(potentialString) === '[object String]';
-}
+    result = RULE_ENGINE.createCharacterCollection().add(gandalf).add(legolas).add(gimli);
+    return result;
+  })();
 
-function isIntegerGreaterZero(potentialInt) {
-  return (typeof potentialInt === 'number') // object of type 'Number'?
-    && !window.isNaN(potentialInt) // check for 'NaN' (actually, "potentialInt % 1 === 0" does the job, too!)
-    && window.isFinite(potentialInt) // check for 'Number.POSITIVE_INFINITY' and 'Number.NEGATIVE_INFINITY' (actually, "potentialInt % 1 === 0" does the job, too!)
-    && (potentialInt % 1 === 0) // since "1.00042 modulo 1" gives you '0.00042...' (where '...' is "machine dependant random float stuff" ;-)
-    && (potentialInt > 0);
-}
 
-function throwIfNoString(potentialString) {
-  if (!isString(potentialString)) {
-    throw new Error("'"+potentialString+"' is not a 'String'.");
+
+  //
+  // member properties - variables and functions - exported
+  // in the Scope of the 'CharacterGeneratorCtrl'
+  //
+
+  /**
+   * List of available, numerical levels.
+   *
+   * @type {Array}
+   */
+  $scope.availableLevels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+  /**
+   * List of available class IDs.
+   *
+   * @type {Array}
+   */
+  $scope.availableClassIds = CharacterClass.prototype.classIdsAsArray();
+
+  /**
+   * View of the list of created characters.
+   *
+   * @type {Array}
+   */
+  $scope.characters = characterCollection.toArray();
+
+  /**
+   *
+   * Creates a new character depending on form input.
+   *
+   * @param formCharacterModel the character model of the &lt;form&gt; input in the UI
+   */
+  $scope.createCharacter = function(formCharacterModel){
+    try {
+      validate(formCharacterModel); // TODO Throwing Errors seems not appropriate for invalid user input. Rethink!
+      createCharacterFrom(formCharacterModel);
+    } catch (err) {
+      logError(err);
+    }
+
+    $scope.characters = characterCollection.toArray();
+  };
+
+  /**
+   * Validates the input data submitted by the website visitor.
+   *
+   * Note that this function is private to the controller (since not exported via '$scope')!
+   *
+   * @param formCharacterModel the form input holding the model of a character
+   * @throws Error If 'formCharacterModel' did not provide the required data
+   */
+  function validate(formCharacterModel) {
+    var requiredPropertyMissing;
+
+    if (!formCharacterModel) {
+      throw new Error("'formCharacterModel' is undefined (= no form input given).");
+    }
+
+    requiredPropertyMissing = !formCharacterModel.characterName || !formCharacterModel.classId || !formCharacterModel.level;
+    if (requiredPropertyMissing) {
+      throw new Error("Cannot create character: Required property missing.");
+    }
   }
-  return potentialString;
-}
 
-function throwIfNoIntegerGreaterZero(potentialInt) {
-  if (!isIntegerGreaterZero(potentialInt)) {
-    throw new Error("'"+potentialInt+"' is not an Integer greater zero.");
+  /**
+   * Creates a new character based on the given form input.
+   *
+   * Note that this function is private to the controller (since not exported via '$scope')!
+   *
+   * @param formCharacterModel The form input holding the model of a character
+   * @throws Error If 'formCharacterModel' did not provide the required data
+   */
+  function createCharacterFrom(formCharacterModel) {
+    var intLevel = parseInt(formCharacterModel.level, 10); // '10' is the radix (= dt. "dezimal system")
+    var newCharacter = RULE_ENGINE.createCharacter(formCharacterModel.characterName, formCharacterModel.classId, intLevel);
+    characterCollection.add(newCharacter);
   }
-  return potentialInt;
-}
 
-/**
- * Iterates over properties of an object and executes a callback for each value.
- *
- * TODO input validation
- *
- * @param obj The object that gets iterated over
- * @param callback The callback function (signature: <code>function(propertyValue)</code>) being applied to each of the object's property values
- */
-function forEachPropertyIn(obj, callback) {
-  var propertyName
-    , propertyValue;
-  for (propertyName in obj) { // iterate over the properties (=keys) of the object
-    propertyValue = obj[propertyName];
-    callback(propertyValue);
+  /**
+   * Converts a character class ID to the class label.
+   *
+   * @param classId of the character class
+   */
+  $scope.classIdToClassLabel = function(classId) {
+    return CharacterClass.prototype.displayNameForClassId(classId);
   }
-}
-
-/**
- * Iterates over properties of an object and returns their values as an Array.
- *
- * TODO input validation
- *
- * @param obj The object that gets iterated over
- */
-function valuesOf(obj) {
-  var result = [];
-  forEachPropertyIn(obj, function(propertyValue){
-    result.push(propertyValue);
-  });
-  return result;
 }
 
 
@@ -82,12 +117,35 @@ function valuesOf(obj) {
 // ------------------------------------------------------------------------
 
 /**
+ * The Rule Engine acts as a facade to its clients: It bundles all operations one can think
+ * of regarding Characters (and other ADnD/RPG stuff) and hides it behind a single API. The
+ * clients don't know the implementation details of {@link Character}, they just see this
+ * API! (TODO create a module for the Rule Engine and all related domain objects)
+ *
+ * (Here we create a singleton: We will only need rule engine for our entire application! The
+ *  singleton is established simply as a collection of function properties assigned to an
+ *  object.)
+ *
+ * @type {Object}
+ */
+var RULE_ENGINE = {
+  // TODO add doc
+  createCharacter: function(charName, charClassId, charLevel) {
+    return new Character(charName, charClassId, charLevel); // TODO input validate TODO error handling(?)
+  },
+
+  // TODO add doc
+  createCharacterCollection: function() {
+    return new CharacterCollection();
+  }
+};
+
+/**
  * Represents the class/type of a playable character, e.g. 'Fighter'.
  *
  * @constructor
  */
 function CharacterClass() {} // ATM, we only use this "class" via its prototype members
-
 /**
  * The unique identifiers of a playable character class.
  *
@@ -210,140 +268,81 @@ function CharacterCollection() {
   this.toArray = toArray;
 }
 
+
+
+// ------------------------------------------------------------------------
+// Global Helper functions.
+// ------------------------------------------------------------------------
+
+function logError(error) {
+  console.log('[ERROR][CharacterGeneratorCtrl] ' + error.message);
+}
+
+function timestamp() {
+  return new Date().getTime();
+}
+
 /**
- * The Rule Engine acts as a facade to its clients: It bundles all operations one can think
- * of regarding Characters (and other ADnD/RPG stuff) and hides it behind a single API. The
- * clients don't know the implementation details of {@link Character}, they just see this
- * API! (TODO create a module for the Rule Engine and all related domain objects)
- *
- * (Here we create a singleton: We will only need rule engine for our entire application! The
- *  singleton is established simply as a collection of function properties assigned to an
- *  object.)
- *
- * @type {Object}
+ * @param potentialString Variable whose type gets checked
+ * @returns {boolean} TRUE, if 'potentialString' is a 'String'
  */
-var RULE_ENGINE = {
-  // TODO add doc
-  createCharacter: function(charName, charClassId, charLevel) {
-    return new Character(charName, charClassId, charLevel); // TODO input validate TODO error handling(?)
-  },
+function isString(potentialString) {
+  // 'Object.prototype.toString(...)' is used here, since we want to use the
+  // native/ 'toString()' instead of a potentially existing, custom
+  // toString()-method!
+  return Object.prototype.toString.call(potentialString) === '[object String]';
+}
 
-  // TODO add doc
-  createCharacterCollection: function() {
-    return new CharacterCollection();
+function isIntegerGreaterZero(potentialInt) {
+  return (typeof potentialInt === 'number') // object of type 'Number'?
+    && !window.isNaN(potentialInt) // check for 'NaN' (actually, "potentialInt % 1 === 0" does the job, too!)
+    && window.isFinite(potentialInt) // check for 'Number.POSITIVE_INFINITY' and 'Number.NEGATIVE_INFINITY' (actually, "potentialInt % 1 === 0" does the job, too!)
+    && (potentialInt % 1 === 0) // since "1.00042 modulo 1" gives you '0.00042...' (where '...' is "machine dependant random float stuff" ;-)
+    && (potentialInt > 0);
+}
+
+function throwIfNoString(potentialString) {
+  if (!isString(potentialString)) {
+    throw new Error("'"+potentialString+"' is not a 'String'.");
   }
-};
+  return potentialString;
+}
 
-
-
-// ------------------------------------------------------------------------
-// The controller for handling form input.
-// ------------------------------------------------------------------------
-
-function CharacterGeneratorCtrl($scope) {
-  //
-  // private controller state
-  //
-
-  /**
-   * Collection of characters created with this controller.
-   */
-  var characterCollection = (function(){ // Function gets executed immediately!
-    var result;
-    var gandalf = RULE_ENGINE.createCharacter('Gandalf', CharacterClass.prototype.CLASS_ID.mage, 20);
-    var gimli = RULE_ENGINE.createCharacter('Gimli', CharacterClass.prototype.CLASS_ID.fighter, 18);
-    var legolas = RULE_ENGINE.createCharacter('Legolas', CharacterClass.prototype.CLASS_ID.thief, 18);
-
-    result = RULE_ENGINE.createCharacterCollection().add(gandalf).add(legolas).add(gimli);
-    return result;
-  })();
-
-
-
-  //
-  // member properties - variables and functions - exported
-  // in the Scope of the 'CharacterGeneratorCtrl'
-  //
-
-  /**
-   * List of available, numerical levels.
-   *
-   * @type {Array}
-   */
-  $scope.availableLevels = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-
-  /**
-   * List of available class IDs.
-   *
-   * @type {Array}
-   */
-  $scope.availableClassIds = CharacterClass.prototype.classIdsAsArray();
-
-  /**
-   * View of the list of created characters.
-   *
-   * @type {Array}
-   */
-  $scope.characters = characterCollection.toArray();
-
-  /**
-   *
-   * Creates a new character depending on form input.
-   *
-   * @param formCharacterModel the character model of the &lt;form&gt; input in the UI
-   */
-  $scope.createCharacter = function(formCharacterModel){
-    try {
-      validate(formCharacterModel); // TODO Throwing Errors seems not appropriate for invalid user input. Rethink!
-      createCharacterFrom(formCharacterModel);
-    } catch (err) {
-      logError(err);
-    }
-
-    $scope.characters = characterCollection.toArray();
-  };
-
-  /**
-   * Validates the input data submitted by the website visitor.
-   *
-   * Note that this function is private to the controller (since not exported via '$scope')!
-   *
-   * @param formCharacterModel the form input holding the model of a character
-   * @throws Error If 'formCharacterModel' did not provide the required data
-   */
-  function validate(formCharacterModel) {
-    var requiredPropertyMissing;
-
-    if (!formCharacterModel) {
-      throw new Error("'formCharacterModel' is undefined (= no form input given).");
-    }
-
-    requiredPropertyMissing = !formCharacterModel.characterName || !formCharacterModel.classId || !formCharacterModel.level;
-    if (requiredPropertyMissing) {
-      throw new Error("Cannot create character: Required property missing.");
-    }
+function throwIfNoIntegerGreaterZero(potentialInt) {
+  if (!isIntegerGreaterZero(potentialInt)) {
+    throw new Error("'"+potentialInt+"' is not an Integer greater zero.");
   }
+  return potentialInt;
+}
 
-  /**
-   * Creates a new character based on the given form input.
-   *
-   * Note that this function is private to the controller (since not exported via '$scope')!
-   *
-   * @param formCharacterModel The form input holding the model of a character
-   * @throws Error If 'formCharacterModel' did not provide the required data
-   */
-  function createCharacterFrom(formCharacterModel) {
-    var intLevel = parseInt(formCharacterModel.level, 10); // '10' is the radix (= dt. "dezimal system")
-    var newCharacter = RULE_ENGINE.createCharacter(formCharacterModel.characterName, formCharacterModel.classId, intLevel);
-    characterCollection.add(newCharacter);
+/**
+ * Iterates over properties of an object and executes a callback for each value.
+ *
+ * TODO input validation
+ *
+ * @param obj The object that gets iterated over
+ * @param callback The callback function (signature: <code>function(propertyValue)</code>) being applied to each of the object's property values
+ */
+function forEachPropertyIn(obj, callback) {
+  var propertyName
+    , propertyValue;
+  for (propertyName in obj) { // iterate over the properties (=keys) of the object
+    propertyValue = obj[propertyName];
+    callback(propertyValue);
   }
+}
 
-  /**
-   * Converts a character class ID to the class label.
-   *
-   * @param classId of the character class
-   */
-  $scope.classIdToClassLabel = function(classId) {
-    return CharacterClass.prototype.displayNameForClassId(classId);
-  }
+/**
+ * Iterates over properties of an object and returns their values as an Array.
+ *
+ * TODO input validation
+ *
+ * @param obj The object that gets iterated over
+ */
+function valuesOf(obj) {
+  var result = [];
+  forEachPropertyIn(obj, function(propertyValue){
+    result.push(propertyValue);
+  });
+  return result;
 }
